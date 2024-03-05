2664ff23779123a698d9e842769192d92b00ba65
Zihua Liu <zliu@ok.sc.e.titech.ac.jp>
2024-03-06 02:11:03 +0900

    add

--- evaluations/simple_unet_two_stage/View_Independent/super_resolution_run.py
+++ evaluations/simple_unet_two_stage/View_Independent/super_resolution_run.py
@@ -3,7 +3,7 @@ from diffusers import StableDiffusionControlNetPipeline, DiffusionPipeline, Stab
 
 from diffusers.schedulers import UniPCMultistepScheduler, DPMSolverMultistepScheduler, EulerDiscreteScheduler, DPMSolverMultistepInverseScheduler, DDIMScheduler, DDIMInverseScheduler
 import sys
-sys.path.append("../../../..")
+sys.path.append("../../..")
 from PIL import Image
 from Inference.simple_unet_two_stage.SD20.View_Independent.SDConImg2ImgNoiseInverseMultiDiffusionPipeline import SDConImg2ImgNoiseInversionMultiDiffusionPipeline
 from Inference.simple_unet_two_stage.SD20.View_Independent.controlnet_tile import ControlNetTileModel
@@ -16,6 +16,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 import skimage.io
 
+
+def find_deepest_folders(root_folder):
+    deepest_folders = []
+    for folder, subfolders, files in os.walk(root_folder):
+        if not subfolders:
+            deepest_folders.append(os.path.abspath(folder))
+    return deepest_folders
 
 def resize_for_condition_image(input_image: Image, resolution: int=512):
     input_image = input_image.convert("RGB")
@@ -54,17 +61,19 @@ def main():
         help="pretrained  unet model path from hugging face or local dir",
     )
     parser.add_argument(
-        "--input_image_path_folder",
+        "--source_folder",
         type=str,
-        default="images",
-        help="KITTI Folder Path ",
-    ) 
+        default="Vhey/a-zovya-photoreal-v2",
+        help="pretrained  unet model path from hugging face or local dir",
+    )
     parser.add_argument(
-        "--output_image_path_folder",
+        "--output_folder",
         type=str,
-        default="images",
-        help="KITTI Folder Path ",
-    ) 
+        default="Vhey/a-zovya-photoreal-v2",
+        help="pretrained  unet model path from hugging face or local dir",
+    )
+    
+
     parser.add_argument(
         "--requested_steps",
         type=int,
@@ -85,6 +94,7 @@ def main():
         default=0.2,
         help="pretrained  unet model path from hugging face or local dir",
     )
+    
 
 
     args = parser.parse_args()
@@ -107,77 +117,88 @@ def main():
     
     
     print("loaded the pretrained model.")
-    '''Doing the Inference at Here'''
-    guided_images = []
-    saved_names = []
     
-    images_folder = args.input_image_path_folder
+    os.makedirs(args.output_folder,exist_ok=True)
+    source_folder = args.source_folder
     
-    os.makedirs(args.output_image_path_folder,exist_ok=True)
-    for file in os.listdir(images_folder):
-        image_path = os.path.join(args.input_image_path_folder,file)
-        input_left = Image.open(image_path)
-        original_size = input_left.size
+    source_folders = find_deepest_folders(source_folder)
+
+    
+    for folder in tqdm(source_folders):
         
-        img_left_left = resize_for_condition_image(input_left)
-        guided_images.append(img_left_left)
-        basename = os.path.basename(image_path)
-        saved_name = os.path.join(args.output_image_path_folder,basename)
-        saved_names.append(saved_name)
+        if len(os.listdir(folder))==0:
+            continue
+        
+        instance_output_folder = folder.replace(args.source_folder,args.output_folder)
+        os.makedirs(instance_output_folder,exist_ok=True)
+
+        '''Doing the Inference at Here'''
+        guided_images = []
+        saved_names = []
+
+        for file in os.listdir(folder):
+            image_path = os.path.join(folder,file)
+            input_left = Image.open(image_path)
+            original_size = input_left.size
+            
+            img_left_left = resize_for_condition_image(input_left)
+            guided_images.append(img_left_left)
+            
+            image_path_enhanced = image_path.replace(args.source_folder,args.output_folder)
+            saved_names.append(image_path_enhanced)
         
     
-    prompt = ["best quality"] * len(guided_images)
-    negative_prompt = ["blur, lowres, bad anatomy, bad hands, cropped, worst quality"] * len(guided_images)
+            prompt = ["best quality"] * len(guided_images)
+            negative_prompt = ["blur, lowres, bad anatomy, bad hands, cropped, worst quality"] * len(guided_images)
+            generator = torch.manual_seed(12345)
+            view_batch_size = 2
+            use_cross_view_att = False
+            sub_batch_bmm = None
 
-    generator = torch.manual_seed(12345)
-    view_batch_size = 2
-    use_cross_view_att = False
-    sub_batch_bmm = None
+            if use_cross_view_att:
+                pipe.unet.set_attn_processor(CrossFrameAttnProcessor(video_length=len(guided_images), group_size=5, sub_batch_bmm=sub_batch_bmm))
+                pipe.controlnet.set_attn_processor(CrossFrameAttnProcessor(video_length=len(guided_images), group_size=5, sub_batch_bmm=sub_batch_bmm))
+            else:
+                view_batch_size *= 8
 
-    if use_cross_view_att:
-        pipe.unet.set_attn_processor(CrossFrameAttnProcessor(video_length=len(guided_images), group_size=5, sub_batch_bmm=sub_batch_bmm))
-        pipe.controlnet.set_attn_processor(CrossFrameAttnProcessor(video_length=len(guided_images), group_size=5, sub_batch_bmm=sub_batch_bmm))
-    else:
-        view_batch_size *= 8
-
-    inv_latents = pipe.invert(
-                prompt=prompt,
-                negative_prompt=negative_prompt,
-                image=guided_images,
-                strength=strength,
-                generator=generator,
-                num_inference_steps=int(requested_steps_inverse / min(strength, 0.999)) if strength > 0 else 0,
-                width=guided_images[0].size[0],
-                height=guided_images[0].size[1],
-                guidance_scale=7,
-                view_batch_size=view_batch_size,
-                circular_padding=True
-            )
+            inv_latents = pipe.invert(
+                        prompt=prompt,
+                        negative_prompt=negative_prompt,
+                        image=guided_images,
+                        strength=strength,
+                        generator=generator,
+                        num_inference_steps=int(requested_steps_inverse / min(strength, 0.999)) if strength > 0 else 0,
+                        width=guided_images[0].size[0],
+                        height=guided_images[0].size[1],
+                        guidance_scale=7,
+                        view_batch_size=view_batch_size,
+                        circular_padding=True
+                    )
 
 
-    result = pipe(prompt=prompt, 
-                negative_prompt=negative_prompt, 
-                # image=guided_images, 
-                image=inv_latents,
-                control_image=guided_images, 
-                width=guided_images[0].size[0],
-                height=guided_images[0].size[1],
-                strength=strength,
-                guidance_scale=7,
-                controlnet_conditioning_scale=1.,
-                generator=generator,
-                num_inference_steps=int(requested_steps / min(strength, 0.999)) if strength > 0 else 0,
-                guess_mode=True,
-                view_batch_size=view_batch_size,
-                circular_padding=True
-                ).images
+            result = pipe(prompt=prompt, 
+                        negative_prompt=negative_prompt, 
+                        # image=guided_images, 
+                        image=inv_latents,
+                        control_image=guided_images, 
+                        width=guided_images[0].size[0],
+                        height=guided_images[0].size[1],
+                        strength=strength,
+                        guidance_scale=7,
+                        controlnet_conditioning_scale=1.,
+                        generator=generator,
+                        num_inference_steps=int(requested_steps / min(strength, 0.999)) if strength > 0 else 0,
+                        guess_mode=True,
+                        view_batch_size=view_batch_size,
+                        circular_padding=True
+                        ).images
+            
     
-    
-    resize_results = [data.resize(original_size) for data in result]
+            resize_results = [data.resize(original_size) for data in result]
 
-    for idx in range(len(resize_results)):
-        saved_name = saved_names[idx]
-        imageio.imsave(saved_name,resize_results[idx])
+            for idx in range(len(resize_results)):
+                saved_name = saved_names[idx]
+                imageio.imsave(saved_name,resize_results[idx])
 
 
 
---
+++ scripts/evaluations/eval_simple_unet_two_stage.sh
@@ -0,0 +1,28 @@
+EVAL_SIMPLE_UNET_Two_Stage_SD20(){
+
+cd ../..
+cd /home/zliu/ACMMM2024/DiffusionMultiBaseline/evaluations/simple_unet_two_stage/View_Independent
+
+controlnet_model_path="/home/zliu/ACMMM2024/DiffusionMultiBaseline/pretrained_models/UNet_Simple_Two_Stage/SD20"
+unet_path="Vhey/a-zovya-photoreal-v2"
+requested_steps=32
+requested_steps_inverse=10
+strength=0.2
+source_folder="/media/zliu/data12/dataset/KITTI/val/Simple_SD20/"
+output_folder="/media/zliu/data12/dataset/KITTI/val/Simple_SD20_Enhanced/"
+
+
+
+CUDA_VISIBLE_DEVICES=0 python super_resolution_run.py \
+            --controlnet_model_path $controlnet_model_path \
+            --unet_path $unet_path \
+            --requested_steps $requested_steps \
+            --requested_steps_inverse $requested_steps_inverse \
+            --strength $strength \
+            --output_folder $output_folder \
+            --source_folder $source_folder
+
+}
+
+
+EVAL_SIMPLE_UNET_Two_Stage_SD20
